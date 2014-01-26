2b0dc260d2ab0d351c28d781ef922b3498f8e21f
Romuald Alapide <schumissimo@msn.com>
2014-01-26 13:46:21 +0100

    main,

--- src/main/java/main/Wator.java
+++ src/main/java/main/Wator.java
@@ -11,7 +11,8 @@ public class Wator {
 
 	public static void main(final String[] args) throws InterruptedException {
 
-		final int tailleEnv = 50;
+		final int tailleEnv = 75;
+
 		final int tailleVue = 600;
 		final int tailleCase = 5;
 
@@ -20,7 +21,8 @@ public class Wator {
 		final int reproductionShark = 8;
 		final int reproductionTuna = 5;
 
-		final int initialNumberOfShark = 100;
+		final int initialNumberOfShark = 200;
+
 		final int initialNumberOfTuna = 800;
 
 		final long pourcentageAffichage = 10;
