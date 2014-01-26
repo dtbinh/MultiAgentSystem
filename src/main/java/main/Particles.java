ce925c0ead2ef379c506d15a5684a3da7ce0cc1d
Romuald Alapide <schumissimo@msn.com>
2014-01-26 13:45:41 +0100

    main

--- src/main/java/main/Particles.java
+++ src/main/java/main/Particles.java
@@ -2,7 +2,6 @@ package main;
 
 import particles.Plateau;
 import particles.StatParticules;
-import wator.WatorStat;
 import core.Environnement;
 import core.Statistique;
 import core.Systeme;
@@ -10,7 +9,7 @@ import core.Vue;
 
 public class Particles {
 	public static void main(final String[] args) throws InterruptedException {
-		final int tailleEnv = 75;
+		final int tailleEnv = 5;
 		final int tailleVue = 600;
 		final int tailleCase = 15;
 		final long pourcentageAffichage = 10;
